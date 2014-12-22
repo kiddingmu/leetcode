104d0af266cf1e889e21c0d0a7ac9a46bdb95c81
kiddingmu <shunanfei@gmail.com>
2014-12-22 20:17:51 +0800

    not use space but the runtime is not small

--- 67_palindrome_number/ispalindrome.py
+++ 67_palindrome_number/ispalindrome.py
@@ -1,20 +1,24 @@
 class Solution:
     # @return a boolean
     def isPalindrome(self, x):
-        """
         # Time Limit Exceeded
         palindrom_val = 0
+        flag = 1 if x > 0 else -1
+        if flag == -1:
+            return False
         original = x
+        x = abs(x)
         while x:
             palindrom_val = palindrom_val * 10 + x % 10
             x = x / 10
-        return palindrom_val == original
-        """
+
+        return flag*palindrom_val == original
         """
         # Accepted
         xstr = str(x)
         xreversed = xstr[::-1]
         return xstr == xreversed
+        """
         """
         xstr = str(x)
         size = len(xstr)
@@ -23,9 +27,10 @@ class Solution:
                 return False
         else:
             return True
+        """
 
 if __name__ == "__main__":
-    x = 1001
+    x = 10001
     solution = Solution()
     print solution.isPalindrome(x)
 
